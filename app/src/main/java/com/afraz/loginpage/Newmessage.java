5199af3f2f3c4068935cafeab53f36c8ddb1b7e2
afraz <afrazafey1523@gmail.com>
2021-06-07 11:43:09 +0500

    3

---
+++ app/src/main/java/com/afraz/loginpage/Newmessage.java
@@ -0,0 +1,92 @@
+package com.afraz.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Newmessage extends AppCompatActivity {
+    EditText txt_pNumber,txt_message;
+    Button btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt_message = findViewById(R.id.txt_message);
+        txt_pNumber = findViewById(R.id.txt_phone_number);
+        btn = findViewById(R.id.Send);
+
+        huhdha();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    huhdha();
+                    btn_send();
+                    hideSoftKeyboard(Newmessage.this);
+                    txt_message.setText("");
+                    txt_pNumber.setText("");
+
+                } catch (Exception e){
+                    Toast.makeText(Newmessage.this, "Fill all fields", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) activity.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        if(inputMethodManager.isAcceptingText()){
+            inputMethodManager.hideSoftInputFromWindow(
+                    activity.getCurrentFocus().getWindowToken(),
+                    0
+            );
+        }
+    }
+
+    private void huhdha(){
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
+
+        if (permissionCheck!= PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS},0);
+        }
+    }
+
+    private void btn_send() {
+        String phoneNumber = txt_pNumber.getText().toString().trim();
+        String Meessage = txt_message.getText().toString().trim();
+
+        if (txt_pNumber.getText().toString().equals("") || !txt_message.getText().toString().equals("")) {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNumber, null, Meessage, null, null);
+
+            Toast.makeText(this, "Message sent", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Please Enter Number or Message", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+}
+
--- app/src/main/java/com/afraz/loginpage/SecondActivity.java
+++ app/src/main/java/com/afraz/loginpage/SecondActivity.java
@@ -2,13 +2,30 @@ package com.afraz.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Button New;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        New = (Button) findViewById(R.id.etNew);
+
+        New.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, Newmessage.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
